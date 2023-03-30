dde5e32c0d4fa6d28b8821d2064015750f73f1c6
JIYEONGSTAR <jiyeongstar@gmail.com>
2023-03-30 21:41:33 +0900

    feat: favicon추가

--- components/Seo.tsx
+++ components/Seo.tsx
@@ -1,10 +1,11 @@
 // 화면 타이틀
 import React from "react";
 import Head from "next/head";
-const Seo = ({ title }: { title: string }) => {
+const Seo = ({ title }: { title?: string }) => {
   return (
     <Head>
-      <title>{title} | MEALTI</title>
+      <link rel="shortcut icon" href="/favicon.ico" />
+      <title>{title && `${title} |`} MEALTI</title>
     </Head>
   );
 };
