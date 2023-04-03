31190b074ff428d54c449b4d14ca4d5075e90993
JIYEONGSTAR <jiyeongstar@gmail.com>
2023-04-04 00:47:58 +0900

    style: 하단 Nav 스타일 변경

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,12 +5,17 @@ import { useRouter } from "next/router";
 import styled, { css } from "styled-components";
 import ThemeToggle from "./ui/ThemeToggle";
 import LargeText from "./ui/LargeText";
-import PersonIcon from "@mui/icons-material/Person";
+
 import ButtonForm from "components/ui/ButtonForm";
 
 import useCurrentUser from "hooks/useCurrentUser";
 import { fireAuth } from "firebase/clientApp";
 import { ThemeContext } from "styled-components";
+import HomeIcon from "@mui/icons-material/Home";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import OutdoorGrillIcon from "@mui/icons-material/OutdoorGrill";
+import PersonIcon from "@mui/icons-material/Person";
 const NavBar = () => {
   const theme = useContext(ThemeContext);
   const { setCurrentUser } = useCurrentUser();
@@ -45,35 +50,55 @@ const NavBar = () => {
         </ButtonWrapper>
       </NavTopWrapper>
       <NavWrapper>
-        <LinkWrapper active={router.pathname === "/" ? "active" : ""} href="/">
-          Home
-        </LinkWrapper>
-        <LinkWrapper
-          active={router.pathname === "/meal" ? "active" : ""}
-          href="/meal"
-        >
-          Meal
-        </LinkWrapper>
-        <LinkWrapper
-          active={router.pathname === "/register" ? "active" : ""}
-          href="/register"
-        >
-          +
-        </LinkWrapper>
-        <LinkWrapper
-          active={router.pathname === "/recipe" ? "active" : ""}
-          href="/recipe"
-        >
-          recipe
-        </LinkWrapper>
-
-        <LinkWrapper
-          active={router.pathname === "/mypage" ? "active" : ""}
-          href="/mypage"
-        >
-          <PersonIcon />
-        </LinkWrapper>
-        <ThemeToggle />
+        <ul>
+          <LinkContainer active={router.pathname === "/" ? "active" : ""}>
+            <LinkWrapper
+              href="/"
+              active={router.pathname === "/" ? "active" : ""}
+            >
+              <HomeIcon />
+              <span>Home</span>
+            </LinkWrapper>
+          </LinkContainer>
+          <LinkContainer active={router.pathname === "/meal" ? "active" : ""}>
+            <LinkWrapper
+              href="/meal"
+              active={router.pathname === "/meal" ? "active" : ""}
+            >
+              <CalendarMonthIcon />
+              <span>Meal</span>
+            </LinkWrapper>
+          </LinkContainer>
+          <LinkContainer
+            active={router.pathname === "/register" ? "active" : ""}
+          >
+            <LinkWrapper
+              href="/register"
+              active={router.pathname === "/register" ? "active" : ""}
+            >
+              <AddCircleIcon /> <span>+</span>
+            </LinkWrapper>
+          </LinkContainer>
+          <LinkContainer active={router.pathname === "/recipe" ? "active" : ""}>
+            <LinkWrapper
+              href="/recipe"
+              active={router.pathname === "/recipe" ? "active" : ""}
+            >
+              <OutdoorGrillIcon />
+              <span>recipe</span>
+            </LinkWrapper>
+          </LinkContainer>
+          <LinkContainer active={router.pathname === "/mypage" ? "active" : ""}>
+            <LinkWrapper
+              href="/mypage"
+              active={router.pathname === "/mypage" ? "active" : ""}
+            >
+              <PersonIcon />
+              <span>mypage</span>
+            </LinkWrapper>
+          </LinkContainer>
+          <ThemeToggle />
+        </ul>
       </NavWrapper>
     </Nav>
   );
@@ -84,12 +109,12 @@ export default NavBar;
 const Nav = styled.nav`
   display: flex;
   width: 100%;
-
   flex-direction: column;
   justify-content: center;
   align-items: center;
   /* box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, */
   /* rgba(0, 0, 0, 0.3) 0px 30px 60px -30px; */
+
   background-color: transparent;
 `;
 const NavTopWrapper = styled.div`
@@ -99,32 +124,92 @@ const NavTopWrapper = styled.div`
   align-items: center;
   padding: 1rem 1.5rem 0 1.5rem;
 `;
+
 const NavWrapper = styled.div`
-  bottom: 0;
   position: fixed;
+  bottom: 0;
+  /* background-color: ${(props) => props.theme.color.backgroundColor}; */
+  /* box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
+    rgba(0, 0, 0, 0.3) 0px 30px 60px -30px; */
+  /* padding: 1rem; */
+  /* border-radius: 20px 20px 0 0; */
   display: flex;
-  /* gap: 1rem; */
+  gap: 1rem 0;
   width: 480px;
   justify-content: center;
   align-items: center;
-  background-color: ${(props) => props.theme.color.backgroundColor};
-  padding: 1rem;
-  border-radius: 20px 20px 0 0;
   z-index: 1000;
+  > ul {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    align-items: center;
+  }
 `;
-const LinkWrapper = styled(Link)<{ active: string }>`
+const LinkContainer = styled.li<{ active: string }>`
+  width: 100%;
+  position: relative;
+  list-style: none;
   font-weight: 600;
   font-size: 18px;
-  margin: 0 20px 0 20px;
-  color: ${(props) => props.theme.color.textColor};
+  /* margin: 0 20px 0 20px; */
+  transition: all 0.3s ease-in-out;
+  /* ${(props) =>
+    props.active === "active" &&
+    css`
+      &::before {
+        height: 100%;
+        left: -15px;
+        border-radius: 0 0 30px 0;
+        background-color: ${(props) => props.theme.color.backgroundColor};
+      }
+      &::after {
+        height: 100%;
+        right: -15px;
+        border-radius: 0 0 0 30px;
+        background-color: ${(props) => props.theme.color.backgroundColor};
+      }
+    `}; */
+`;
 
+const LinkWrapper = styled(Link)<{ active: string }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  text-decoration: none;
+  width: 100%;
+  height: 100%;
+  line-height: 1.4;
+  :hover {
+    border: 1px solid ${(props) => props.theme.color.subColor};
+    border-radius: 30px 30px 0 0;
+  }
   ${(props) =>
     props.active === "active" &&
     css`
-      text-decoration: underline;
-      text-decoration-color: ${(props) => props.theme.color.subColor};
-      text-underline-offset: 8px;
-    `};
+      ::before,
+      ::after {
+        content: "";
+        position: absolute;
+        background-color: ${props.theme.color.subColor};
+        z-index: -1;
+      }
+      ::before {
+        width: 100%;
+        height: 100%;
+        border-radius: 30px 30px 0 0;
+      }
+      > span,
+      > svg > path {
+        color: ${props.theme.color.backgroundColor};
+      }
+    `}
+
+  & > span {
+    font-size: 0.5rem;
+  }
 `;
 
 const ButtonWrapper = styled.div`
