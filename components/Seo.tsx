50f1dd535c2d3cfabffb8754d5ae425179cb579f
JIYEONGSTAR <jiyeongstar@gmail.com>
2023-05-31 10:39:51 +0900

    feat: seo 설정

--- components/Seo.tsx
+++ components/Seo.tsx
@@ -6,6 +6,17 @@ const Seo = ({ title }: { title?: string }) => {
     <Head>
       <link rel="shortcut icon" href="/favicon.ico" />
       <title>{title && `${title} |`} MEALTI</title>
+      <meta name="description" content="MEALTI - 나만의 식비 가계부" />
+      <meta
+        name="keywords"
+        content="식비, 가계부, 나만의, Mealti, 식사, 비용"
+      />
+      <meta property="og:title" content="MEALTI - 나만의 식비 가계부" />
+      <meta
+        property="og:description"
+        content="MAELTI는 식비 가계부 서비스입니다."
+      />
+      <meta property="og:url" content="https://mealti-38fed.web.app" />
     </Head>
   );
 };
